923bdc956da8019a78de1e40030089836208498a
Raghad Salem <raghadnadersalem20022017@gmail.com>
2024-08-14 15:46:12 +0300

    Initial setup

---
+++ InventoryManagement/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace InventoryManagement
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to our inventory management system");
+        }
+    }
+}
